64efdaafe9a910169b5878e0eb159a89695a4012
Alex Bruno Neri Humberto <alex.humberto@sicoobcredivar.com.br>
2023-02-03 16:47:56 -0300

    Coding Card Info

--- src/components/CardInfo/index.tsx
+++ src/components/CardInfo/index.tsx
@@ -1,9 +1,35 @@
-import { CardInfoContainer } from "./styles";
+import { CardInfoContainer, 
+CardInfoContent,
+CardInfoContentInformations,
+CardInfoContentInformationsName,
+CardInfoContentInformationsDescription,
+CardInfoContentInformationsLast
+} from "./styles";
+import Img from '../../assets/Logo.svg'
+import GitHub from '../../assets/Git.svg'
+import Building from '../../assets/Building.svg'
+import Users from '../../assets/Users.svg'
 
 export default function CardInfo() {
     return (
         <CardInfoContainer>
-                Ola
+            <CardInfoContent>
+                <img src={Img} alt="" />
+                <CardInfoContentInformations>
+                    <CardInfoContentInformationsName>
+                        <span>Álex Bruno Neri Humberto</span>
+                        <p>Github</p>
+                    </CardInfoContentInformationsName>
+                    <CardInfoContentInformationsDescription>
+                        <p>Desenvolvedor Full Stack</p>
+                    </CardInfoContentInformationsDescription>
+                    <CardInfoContentInformationsLast>
+                        <span><img src={GitHub} alt=""/>alexbruno10</span>
+                        <span><img src={Building} alt=""/>Sicoob Credivar</span>
+                        <span><img src={Users} alt=""/>10 seguidores</span>
+                    </CardInfoContentInformationsLast>
+                </CardInfoContentInformations>    
+            </CardInfoContent>
         </CardInfoContainer>
     )
 };
--- src/components/CardInfo/styles.ts
+++ src/components/CardInfo/styles.ts
@@ -12,3 +12,66 @@ export const CardInfoContainer = styled.div`
     margin-top: -5.5rem;
 
 `
+
+export const CardInfoContent = styled.div`
+    display: flex;
+    width: 100%;
+    padding-left: 2.5rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    gap: 2rem;
+
+    img {
+        width: 9.25rem;
+        height: 9.25rem;
+        border-radius: 8px;
+    }
+`
+
+export const CardInfoContentInformations = styled.div`
+    width: 38.31rem;
+    height: 8.56rem;
+
+
+`
+
+export const CardInfoContentInformationsName = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    span {
+        font-style: normal;
+        font-weight: 700;
+        font-size: 1.5rem;
+        line-height: 130%;
+    
+        color: ${({theme}) => theme.colors["title"]};
+    }
+`
+
+export const CardInfoContentInformationsDescription = styled.div`
+    width: 100%;
+    height: 3.25rem;
+    font-size: 1rem;
+    font-style: normal;
+    margin-top: 0.5rem;
+    color: ${({theme}) => theme.colors["text"]};
+
+`
+
+export const CardInfoContentInformationsLast = styled.div`
+    display: flex;
+    gap: 1.5rem;
+
+    span {
+        font-style: normal;
+        font-size: 1rem;
+        line-height: 130%;
+        color: ${({theme}) => theme.colors["text"]};
+    }
+
+    svg {
+        width: 1.125rem;
+        height: 1.125rem;
+    }
+`
