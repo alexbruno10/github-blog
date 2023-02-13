736bd07d34028d6d076a0cd7d2b35a123d36eb75
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2023-02-13 20:11:55 -0300

    Request API with axios

--- src/components/CardProfile/index.tsx
+++ src/components/CardProfile/index.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState, useEffect } from "react";
+import { api } from "../../lib/axios";
 import { CardInfoContainer, 
 CardInfoContent,
 ProfileDetails
@@ -5,32 +7,59 @@ ProfileDetails
 import Img from '../../assets/Teste.svg'
 import { GithubLogo, Buildings, Users, ArrowSquareOut} from "phosphor-react";
 
+type GithubProfile = {
+    name: string
+    login: string
+    avatar_url: string
+    company: string | null
+    followers: number
+    bio: string
+  }
+
 export default function CardProfile() {
+
+    const [profile, setProfile] = useState<GithubProfile>({} as GithubProfile);
+
+    const getProfileData = useCallback(async () => {
+        try {
+          const response = await api.get(`/users/alexbruno10`);
+    
+          setProfile(response.data);
+        } catch {
+          console.log('Erro')
+        }
+      }, [profile]);
+
+    
+    useEffect(() => {
+    getProfileData();
+    }, []);
+
     return (
         <CardInfoContainer>
             <CardInfoContent>
-                <img src={Img} alt="" />
+                <img src={profile.avatar_url} alt="" />
                 <ProfileDetails>
                     <header>
-                    <h1>Alex Bruno Neri Humberto</h1>
+                    <h1>{profile.name}</h1>
 
                     <a href="https://github.com/alexbruno10" target="_blank">GITHUB 
                     <ArrowSquareOut size={12} weight="fill"/>
                     </a>
                     </header>
-                    <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+                    <p>{profile.bio}</p>
                     <ul>
                         <li>
                             <GithubLogo size={32} weight="fill"/>
-                            alexbruno10
+                            {profile.login}
                         </li>
                         <li>
                             <Buildings size={32} weight="fill"/>
-                            Sicoob
+                            {profile.company}
                         </li>
                         <li>
                             <Users size={32} weight="fill"/>
-                            10 seguidores
+                            {profile.followers} seguidores
                         </li>
                     </ul>
                 </ProfileDetails> 
